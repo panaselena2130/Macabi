444f325639905c4065e3ad8cea9a93ea02a5d1e4
panaselena2130 <panaselena7@gmail.com>
2023-10-08 21:49:03 +0300

    Initialcommit8

--- pom/home_page.py
+++ pom/home_page.py
@@ -7,8 +7,8 @@ from pom.Base_page import Driver_Base
 class HomePage(Driver_Base):
 
 
-    def __init__(self,driver1):
-        super(HomePage, self).__init__(driver1)
+    def __init__(self,something):
+        super(HomePage, self).__init__(something)
 
 
         self.locator_passport_id = "identifyWithOtpCitizenId"
